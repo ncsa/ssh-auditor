0c0eeafc48010b31fc1a3ae9739c5be5f953dc51
Justin A <justin@t420.bouncybouncy.net>
2017-05-11 20:34:36 -0400

    nicer discover help

--- cmd/discover.go
+++ cmd/discover.go
@@ -12,8 +12,13 @@ var ports []int
 var discoverCmd = &cobra.Command{
 	Use:     "discover",
 	Aliases: []string{"d"},
+	Example: "discover -p 22 -p 2222 192.168.1.0/24 10.1.1.0/24",
 	Short:   "discover new hosts",
 	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			cmd.Usage()
+			return
+		}
 		scanConfig := sshauditor.ScanConfiguration{
 			Include: args,
 			Exclude: []string{}, //FIXME add --exclude option
