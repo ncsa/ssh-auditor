8c75ac18e79625e5030ea90fb62be424f6752c78
Justin Azoff <justin@bouncybouncy.net>
2018-02-25 14:45:31 -0500

    add html report

--- cmd/report.go
+++ cmd/report.go
@@ -2,8 +2,9 @@ package cmd
 
 import (
 	"encoding/json"
-	"html/template"
+	html_template "html/template"
 	"os"
+	text_template "text/template"
 
 	log "github.com/inconshreveable/log15"
 	"github.com/ncsa/ssh-auditor/sshauditor"
@@ -43,7 +44,26 @@ var reportTXTCmd = &cobra.Command{
 			log.Error(err.Error())
 			os.Exit(1)
 		}
-		t := template.Must(template.New("report").Parse(reportTXTTemplate))
+		t := text_template.Must(text_template.New("report").Parse(reportTXTTemplate))
+		err = t.Execute(os.Stdout, report)
+		if err != nil {
+			log.Error(err.Error())
+			os.Exit(1)
+		}
+		return
+	},
+}
+var reportHTMLCmd = &cobra.Command{
+	Use:   "html",
+	Short: "html report",
+	Run: func(cmd *cobra.Command, args []string) {
+		auditor := sshauditor.New(store)
+		report, err := auditor.GetReport()
+		if err != nil {
+			log.Error(err.Error())
+			os.Exit(1)
+		}
+		t := html_template.Must(html_template.New("report").Parse(reportHTMLTemplate))
 		err = t.Execute(os.Stdout, report)
 		if err != nil {
 			log.Error(err.Error())
@@ -55,8 +75,9 @@ var reportTXTCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(reportCmd)
-	reportCmd.AddCommand(reportTXTCmd)
 	reportCmd.AddCommand(reportJSONCmd)
+	reportCmd.AddCommand(reportTXTCmd)
+	reportCmd.AddCommand(reportHTMLCmd)
 }
 
 var reportTXTTemplate = `
@@ -88,4 +109,82 @@ Active Hosts: {{ .ActiveHostsCount }}
 	Seen First {{.SeenFirst}}
 	Seen Last {{.SeenLast}}
 {{end}}
+`
+
+var reportHTMLTemplate = `
+<html>
+<body>
+
+<h1>Vulnerabilities: {{ .VulnerabilitiesCount }}</h1>
+<table>
+<thead>
+	<tr>
+		<th>Host</th>
+		<th>User</th>
+		<th>Password</th>
+		<th>Result</th>
+		<th>Last Tested</th>
+		<th>Version</th>
+	</tr>
+</thead>
+<tbody>
+{{range .Vulnerabilities}}
+<tr>
+	<td> {{.Host.Hostport}} </td>
+	<td> {{.HostCredential.User}} </td>
+	<td> {{.HostCredential.Password}} </td>
+	<td> {{.HostCredential.Result}} </td>
+	<td> {{.HostCredential.LastTested}} </td>
+	<td> {{.Host.Version}} </td>
+</tr>
+{{end}}
+</tbody>
+</table>
+
+<h1>Duplicate Keys: {{ .DuplicateKeysCount }} </h1>
+{{ range $key, $hosts := .DuplicateKeys }}
+<h2> {{$key}} </h2>
+<table>
+<thead>
+	<tr>
+		<th>Host</th>
+		<th>Version</th>
+		<th>Seen First</th>
+		<th>Seen Last</th>
+	</tr>
+</thead>
+<tbody>
+{{ range $hosts }}
+<tr>
+	<td> {{.Hostport}} </td>
+	<td> {{.Version}} </td>
+	<td> {{.SeenFirst}} </td>
+	<td> {{.SeenLast}} </td>
+</tr>
+{{end}}
+</tbody>
+</table>
+{{end}}
+
+<h1> Active Hosts: {{ .ActiveHostsCount }} </h1>
+<table>
+<thead>
+	<tr>
+		<th>Host</th>
+		<th>Version</th>
+		<th>Seen First</th>
+		<th>Seen Last</th>
+	</tr>
+</thead>
+<tbody>
+{{ range .ActiveHosts }}
+<tr>
+	<td> {{.Hostport}} </td>
+	<td> {{.Version}} </td>
+	<td> {{.SeenFirst}} </td>
+	<td> {{.SeenLast}} </td>
+</tr>
+{{end}}
+</tbody>
+</table>
 `
